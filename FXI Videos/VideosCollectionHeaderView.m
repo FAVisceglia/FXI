88f79e08b0f96d3128c730266139ebaaa3cae707
Visceglia Anthony <avisceglia@fxi12585525.home>
2014-12-02 11:28:14 -0500

    Sections commit

--- FXI Videos/VideosCollectionHeaderView.m
+++ FXI Videos/VideosCollectionHeaderView.m
@@ -14,12 +14,34 @@
 {
     [super layoutSubviews];
     
+    // Drop Shadow
     UIBezierPath *shadowPath = [UIBezierPath bezierPathWithRect:self.bounds];
     [[self layer] setMasksToBounds:NO];
     [[self layer] setShadowColor:[UIColor darkGrayColor].CGColor];
     [[self layer] setShadowOffset:CGSizeMake(0.0f, 2.5f)];
     [[self layer] setShadowOpacity:0.5f];
     [[self layer] setShadowPath:shadowPath.CGPath];
+    
+    /**
+    // Border
+    CALayer *bottomBorder = [CALayer layer];
+    CALayer *topBorder = [CALayer layer];
+    CGFloat borderWidth = 0.5f;
+    
+    [bottomBorder setFrame:CGRectMake(0.0f, [self frame].size.height-borderWidth, [self frame].size.width, borderWidth)];
+    [bottomBorder setBackgroundColor:[UIColor colorWithRed:(18.0/255.0)
+                                                     green:(52.0/255.0)
+                                                      blue:(88.0/255.0)
+                                                     alpha:1.0f].CGColor];
+    [[self layer] addSublayer:bottomBorder];
+    
+    [topBorder setFrame:CGRectMake(0.0f, 0.0f, [self frame].size.width, borderWidth)];
+    [topBorder setBackgroundColor:[UIColor colorWithRed:(18.0/255.0)
+                                                  green:(52.0/255.0)
+                                                   blue:(88.0/255.0)
+                                                  alpha:1.0f].CGColor];
+    [[self layer] addSublayer:topBorder];
+     **/
 }
 
 @end
